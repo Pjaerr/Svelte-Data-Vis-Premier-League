17101e79dd0e48190058c1a92c34a0a60ab2cc59
Pjaerr <joshcharlesjackson@gmail.com>
2019-08-26 18:50:46 +0100

    Messing around with SVGs

---
+++ src/components/MapRegion.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let name;
+  export let colour;
+  export let width;
+  export let height;
+  export let posX;
+  export let posY;
+  export let svgPath;
+</script>
+
+<style>
+  .map-region {
+    position: absolute;
+  }
+
+  .map-region > svg {
+    transition: transform 0.2s ease-in;
+  }
+
+  .map-region > svg:hover {
+    transform: scale(1.5);
+  }
+</style>
+
+<div class="map-region" style="left:{posX}%; top:{posY}%;">
+  <svg
+    class="map-region-svg"
+    width="{width}px"
+    height="{height}px"
+    viewBox="0 0 {width}
+    {height}"
+    xmlns="http://www.w3.org/2000/svg">
+    <path fill={colour} d={svgPath} transform="translate({width * 0.5})" />
+  </svg>
+</div>
