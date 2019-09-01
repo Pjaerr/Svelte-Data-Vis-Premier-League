d2994f73e5315b5b103432075c846095bd405bcc
Pjaerr <joshcharlesjackson@gmail.com>
2019-09-01 16:22:50 +0100

    Converted the JSON files to JS files exporting a default object.

---
+++ src/Data/Players.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    name: "Steve Bruce",
+    regions: ["Northumberland"]
+  },
+  {
+    name: "Brian McClair",
+    regions: ["Scotland"]
+  },
+  {
+    name: "Gary Pallister",
+    regions: ["Kent"]
+  },
+  {
+    name: "Ryan Giggs",
+    regions: ["Wales"]
+  },
+  {
+    name: "Mark Hughes",
+    regions: ["Wales"]
+  },
+  {
+    name: "Paul Ince",
+    regions: ["Greater London"]
+  },
+  {
+    name: "Denis Irwin",
+    regions: ["Ireland"]
+  },
+  {
+    name: "Paul Parker",
+    regions: ["Greater London"]
+  },
+  {
+    name: "Darren Ferguson",
+    regions: ["Scotland"]
+  },
+  {
+    name: "Clayton Blackmore",
+    regions: ["Wales"]
+  },
+  {
+    name: "Bryan Robson",
+    regions: ["Durham"]
+  },
+  {
+    name: "Mike Phelan",
+    regions: ["Lancashire"]
+  },
+  {
+    name: "Dion Dublin",
+    regions: ["Leicestershire"]
+  },
+  {
+    name: "Danny Wallace",
+    regions: ["Greater London"]
+  },
+  {
+    name: "Nicky Butt",
+    regions: ["Lancashire", "Cheshire"]
+  },
+  {
+    name: "Neil Webb",
+    regions: ["Berkshire"]
+  },
+  {
+    name: "Lee Sharpe",
+    regions: ["Staffordshire", "Worcestershire", "Warwickshire"]
+  }
+];
