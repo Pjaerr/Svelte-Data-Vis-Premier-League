62ce9a39ce1245f0154c7d16d0de8d1dee3a190f
Pjaerr <joshcharlesjackson@gmail.com>
2019-09-08 17:03:17 +0100

    Implemented the basic <MapRegion> component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
   import MapContainer from "./Components/MapContainer.svelte";
+  import MapRegion from "./Components/MapRegion.svelte";
   import Regions from "./Data/Regions.js";
 </script>
 
 <MapContainer>
   {#each Regions as { name, svgPath }}
-    <path d={svgPath} />
+    <MapRegion
+      {svgPath}
+      fillColour="red"
+      strokeColour="white"
+      strokeWidth="1px" />
   {/each}
 </MapContainer>
---
+++ src/Components/MapRegion.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let svgPath;
+  export let fillColour = "#333";
+  export let strokeColour = "#fff";
+  export let strokeWidth = "1px";
+</script>
+
+<path
+  d={svgPath}
+  fill={fillColour}
+  stroke={strokeColour}
+  style="stroke-width: {strokeWidth}" />
