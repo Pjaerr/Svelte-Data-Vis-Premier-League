2ed92ced8143db2f3e1e3f0ddb25db35cb6221a5
Pjaerr <joshcharlesjackson@gmail.com>
2019-11-18 17:19:12 +0000

    Worked more on the Making Sense of the Data section
    This is a tricky section, the data isn't the most well formatted but I think going through and cleaning up data into something usable is a useful exercise

--- src/App.svelte
+++ src/App.svelte
@@ -3,35 +3,20 @@
 
   import MapContainer from "./Components/MapContainer.svelte";
   import MapRegion from "./Components/MapRegion.svelte";
-  import InfoContainer from "./Components/InfoContainer.svelte";
-  import GeneralInformation from "./Components/GeneralInformation.svelte";
   import RegionInformation from "./Components/RegionInformation.svelte";
   import Regions from "./Data/Regions.js";
 
-  import { NUMBER_OF_PLAYERS } from "./Data/Data.js";
+  import { getRegionData } from "./Data/Data.js";
 
-  let activeRegion;
+  let activeRegion = {
+    name: "No Region"
+  };
   let title = "Premier League: UK & Ireland Contribution";
 </script>
 
 <style>
   .app {
     display: flex;
-  }
-
-  .left-panel {
-    padding-left: 40px;
-    max-width: 50%;
-    flex: 2;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-  }
-
-  .right-panel {
-    display: flex;
-    justify-content: center;
-    flex: 2;
   }
 
   .map-container {
@@ -43,15 +28,6 @@
       flex-direction: column;
     }
 
-    .left-panel {
-      max-width: 100%;
-      padding-left: 0;
-    }
-
-    .right-panel {
-      max-width: 100%;
-    }
-
     .map-container {
       position: relative;
     }
@@ -59,31 +35,18 @@
 </style>
 
 <main class="app">
-  <div class="left-panel">
-    <div class="info-container" transition:fade={{ duration: 1000 }}>
-      <InfoContainer title={activeRegion ? activeRegion : title}>
-        {#if activeRegion}
-          <RegionInformation region={activeRegion} />
-        {:else}
-          <GeneralInformation />
-        {/if}
-      </InfoContainer>
-    </div>
-  </div>
-  <div class="right-panel">
-    <div class="map-container">
-      <MapContainer>
-        {#each Regions as { name, svgPath }}
-          <MapRegion
-            on:click={() => {
-              activeRegion = name;
-            }}
-            {svgPath}
-            fillColour="#333"
-            strokeColour="white"
-            strokeWidth="1px" />
-        {/each}
-      </MapContainer>
-    </div>
+  <div class="map-container">
+    <MapContainer>
+      {#each Regions as { name, svgPath }}
+        <MapRegion
+          on:click={() => {
+            activeRegion = getRegionData(name);
+          }}
+          {svgPath}
+          fillColour={getRegionData(name).colour}
+          strokeColour="white"
+          strokeWidth="1px" />
+      {/each}
+    </MapContainer>
   </div>
 </main>
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,160 +1,108 @@
-/** What conclusions do we need to make?
- * -------------------------------------------
- * * How much has each Region contributed to premier league wins?
- * * How many players from the UK and Ireland have contributed to premier league wins?
- * * What percentage of players who won the premier league were from UK and Ireland?
- * * Which season saw the highest contribution from UK and Ireland players?
- * * Which player from UK and Ireland has contributed to the most premier league wins?
- */
-
-/** Sub-Task List for "Making Sense of the Data" section
- * -------------------------------------------------------
- * 1. Convert the Region objects into something with the following structure:
- *
- *          {
- *              name: string,
- *              players: [
- *                  {
- *                      name: string,
- *                      seasons: [
- *                          {
- *                              year: string,
- *                              team: string,
- *                              appearances: number
- *                          }
- *                      ]
- *                      weighting: number
- *                  }
- *              ]
- *          }
- *
- * 4. Tally up how many players have won the premier league overall and then map the number
- *    of UK and Ireland vs. Foreign players on a 0, 100 range.
- *
- * 5. Go through all of the seasons and remove all non UK and Ireland players. From the remaining players,
- *    order the seasons by number of players. Then out of the seasons, run the weighting functionality and
- *    then store the resulting season year (Eg. "1994/1995") as a constant that is exported showing the
- *    season with the highest UK and Ireland contribution.
- *
- * 6. Go through all of the seasons and remove all non UK and Ireland players. Out of the remaining players
- *    remove any players that haven't won more than a single season. Out of the remaining players tally
- *    up each of their accumlated appearances in each season. At the end, the player with the highest
- *    accumulated total is the player who has contributed the most. Export this as a constant.
- */
-
 import Players from "./Players";
+import Regions from "./Regions";
+import Titles from "./Titles";
 
-//Constants
-export const NUMBER_OF_PLAYERS = Players.length;
+/** Step 1:
+ Convert the Region objects into something more usable by creating a new object for each region
+ with the following structure:
+*          {
+*              name: string,
+*              players: [
+*                  {
+*                      name: string,
+*                      seasons: [
+*                          {
+*                              year: string,
+*                              team: string,
+*                              appearances: number
+*                          }
+*                      ]
+*                  },
+*             appearances: number
+*              ]
+*          }
 
-/**
- *  Calculates which, out of a collection of arrays of objects with an appearance property,
- * has the highest number of appearances by adding them all together and returning the array
- * that has the highest accumulated weighting.
- * @param {[[{appearances: number}]]} arrays
- * @returns {[{accumulatedWeight: number, array: []}]} An array containing an object for each array alongside the accumulated weighting
- */
-export function getHighestAccumulatedAppearances(arrays) {
-  //Store each array in an object alongside their accumulated appearances
-  const accumulatedArrays = arrays
-    .map(array => {
-      let accumulatedAppearances = 0;
+*/
+const regions = Regions.map(region => {
+  //Create the initial region object
+  const newRegionObject = {
+    name: region.name,
+    players: [],
+    appearances: 0
+  };
 
-      array.forEach(obj => {
-        accumulatedAppearances += parseInt(obj.appearances);
-      });
-
-      return {
-        array,
-        accumulatedAppearances
+  //For each of the players
+  for (const player of Players) {
+    //If the player is from this region
+    if (player.regions.includes(region.name)) {
+      //Create the initial player object
+      const playerObject = {
+        name: player.name,
+        seasons: []
       };
-    })
-    .sort((a, b) => b.accumulatedAppearances - a.accumulatedAppearances);
 
-  /** Return the highest weighted array, account for multiple arrays having the same
-   * accumulated weighting and so multiple need to be returned.*/
-  let highestWeightedArrays = [accumulatedArrays[0]];
+      //For each of the titles
+      for (const title of Titles) {
+        //For each of the players who won the title
+        for (const winningPlayer of title.players) {
+          //If the winning player is the same as our player
+          if (player.name === winningPlayer.name) {
+            //Add the current title to the player object
+            playerObject.seasons.push({
+              year: title.season,
+              team: title.winner,
+              appearances: winningPlayer.appearances
+            });
 
-  for (let i = 0; i < accumulatedArrays.length; i++) {
-    if (
-      accumulatedArrays[i].accumulatedAppearances !==
-      accumulatedArrays[i + 1].accumulatedAppearances
-    ) {
-      i = accumulatedArrays.length;
-    } else {
-      highestWeightedArrays.push(accumulatedArrays[i + 1]);
+            //Increment the overall appearances of the region by the player we just added
+            newRegionObject.appearances += winningPlayer.appearances;
+          }
+        }
+      }
+
+      //Add the player to the region object
+      newRegionObject.players.push(playerObject);
     }
   }
 
-  return highestWeightedArrays;
+  return newRegionObject;
+});
+
+/* Step 2:
+  Figure out what the colour of each region should be based on their number of appearances
+*/
+
+//What is the maximum number of appearances a region can have
+let highestNumberOfAppearances = 0;
+
+for (const region of regions) {
+  if (region.appearances > highestNumberOfAppearances) {
+    highestNumberOfAppearances = region.appearances;
+  }
 }
 
-// ! Experimental Code
-// import Players from "./Players.js";
-// import Titles from "./Titles.js";
-// import Regions from "./Regions.js";
+//Map the number of appearances (0 to highestNumberOfAppearances) to an RGB value (50 to 255)
+for (const region of regions) {
+  let greenValue = Math.round(
+    ((region.appearances - 0) / (highestNumberOfAppearances - 0)) * (255 - 30) +
+      30
+  );
 
-// const getContrib = regionToCalculateFor => {
-//   const regions = Regions.map(region => {
-//     return {
-//       name: region.name,
-//       players: []
-//     };
-//   });
+  region.colour = `rgb(0, ${greenValue}, 0)`;
+}
 
-//   Players.forEach(player => {
-//     player.regions.forEach(regionPlayerIsFrom => {
-//       regions.forEach(region => {
-//         if (region.name === regionPlayerIsFrom) {
-//           region.players.push({
-//             name: player.name,
-//             seasons: []
-//           });
-//         }
-//       });
-//     });
-//   });
+/* Step 3:
+  Export a function that takes a region name and will return the region for that name.
+*/
+export const getRegionData = regionName => {
+  let regionToReturn;
 
-//   Titles.forEach(title => {
-//     regions.forEach(region => {
-//       region.players.forEach(player => {
-//         title.players.forEach(winningPlayer => {
-//           if (winningPlayer.name === player.name) {
-//             player.seasons.push({
-//               year: title.season,
-//               team: title.winner,
-//               appearances: winningPlayer.appearances
-//             });
-//           }
-//         });
-//       });
-//     });
-//   });
+  for (const region of regions) {
+    if (region.name === regionName) {
+      regionToReturn = region;
+      break;
+    }
+  }
 
-//   let highestNumberOfPlayers = 0;
-
-//   for (let i = 0; i < regions.length; i++) {
-//     if (regions[i].players.length > highestNumberOfPlayers) {
-//       highestNumberOfPlayers = regions[i].players.length;
-//     }
-//   }
-
-//   let colour = `rgb(0, 50, 0)`;
-//   regions.forEach(region => {
-//     if (region.name === regionToCalculateFor) {
-//       let greenValue = Math.round(
-//         ((region.players.length - 0) / (33 - 0)) * (255 - 50) + 50
-//       );
-
-//       if (region.players.length === 0) {
-//         greenValue = 0;
-//       }
-
-//       colour = `rgb(0, ${greenValue}, 0)`;
-//     }
-//   });
-
-//   return colour;
-// };
-
-// export default getContrib;
+  return regionToReturn;
+};
