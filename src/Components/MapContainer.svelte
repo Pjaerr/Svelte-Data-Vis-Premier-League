9790d7a52b77c88751c11a106d34ab8a90d867d2
Pjaerr <joshcharlesjackson@gmail.com>
2020-01-07 11:15:19 +0000

    Updated SVG resize to happen on window resize.

--- src/Components/MapContainer.svelte
+++ src/Components/MapContainer.svelte
@@ -5,19 +5,21 @@
   let width = "0";
   let height = "0";
 
-  onMount(() => {
+  let windowHeight = 10;
+
+  const resizeSVG = () => {
     let svgBoundingBox = svg.getBBox();
 
     width = svgBoundingBox.x + svgBoundingBox.width + svgBoundingBox.x;
     height = svgBoundingBox.y + svgBoundingBox.height + svgBoundingBox.y;
-  });
+  };
+
+  onMount(resizeSVG);
 </script>
 
-<svg
-  viewBox="0 0 {width}
-  {height}"
-  height={window.innerHeight - 10}
-  bind:this={svg}>
+<svelte:window on:resize={resizeSVG} bind:innerHeight={windowHeight} />
+
+<svg viewBox="0 0 {width} {height}" height={windowHeight - 10} bind:this={svg}>
   <g class="regions">
     <slot />
   </g>
